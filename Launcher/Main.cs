e3c36a360d0467e3068234e8634b7cecf2fc6b05
Raitou Kun <raitoukun@hotmail.com>
2022-08-22 10:01:33 +0800

    Initial Commit

---
+++ Launcher/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Server;
+/***
+ * Base Launcher can be anything so long as you reference the C# Library
+ */
+
+
+namespace Launcher
+{
+    internal class Launcher
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("CounterSharpO2 Project");
+            await TCPServer.Start();
+            
+        }
+    }
+}
---
+++ TCPServer/Startup.cs
@@ -0,0 +1,61 @@
+﻿using DotNetty.Codecs;
+using DotNetty.Handlers.Logging;
+using DotNetty.Handlers.Tls;
+using DotNetty.Transport.Bootstrapping;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Channels.Sockets;
+
+namespace Server
+{
+    public static class TCPServer {
+
+        static bool bRunning = true;
+
+        public static async Task Start()
+        {
+
+            MultithreadEventLoopGroup bossGroup = new MultithreadEventLoopGroup(1);
+            MultithreadEventLoopGroup workerGroup = new MultithreadEventLoopGroup();
+
+            try
+            {
+                // https://github.com/Azure/azure-iot-protocol-gateway/blob/master/docs/DeveloperGuide.md
+                ServerBootstrap serverBootstrap = new ServerBootstrap();
+                serverBootstrap
+                    .Group(bossGroup, workerGroup)
+                    .Channel<TcpServerSocketChannel>()
+                    .ChildHandler(new ActionChannelInitializer<IChannel>(channel =>
+                    {
+                        IChannelPipeline pipeline = channel.Pipeline;
+                    }));
+
+
+                IChannel bootstrapChannel = await serverBootstrap.BindAsync(30001); // TODO: make this in a config
+
+                Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e) {
+                    e.Cancel = true;
+                    Stop();
+                };
+
+                while (bRunning) ;
+                
+                await bootstrapChannel.CloseAsync();
+
+            } catch (Exception ex)
+            {
+               Console.WriteLine(ex.ToString()); // TODO: make a logger
+            } finally
+            {
+                Task.WaitAll(bossGroup.ShutdownGracefullyAsync(), workerGroup.ShutdownGracefullyAsync());
+                Console.WriteLine("exited gracefully"); // TODO: make a logger
+            }
+        }
+
+        public static void Stop()
+        {
+            bRunning = false;
+        }
+
+        
+    }
+}
